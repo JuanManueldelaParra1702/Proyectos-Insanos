21e12d5b3b144dad59816fc78d92c3f6afb81fcb
Juan Manuel <juanmanueldelaparra1702@protonmail.com>
2025-02-20 00:09:33 -0600

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,6 +1,6 @@
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;:,.<>?`~"
+chars = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;:,.<>?`~"
 
 password = ""
 for i in range (16):
